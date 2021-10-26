917ef761cc4290156ab5e7396a185ab35697dcd1
Luke Warddrip <91761003+wardl983@users.noreply.github.com>
2021-10-26 22:16:33 +0000

    adding api call to dog

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello")
+import requests
+
+url_host = "https://dog.ceo"
+endpoint = "/api/breeds/list/all"
+query = ""
+
+url = url_host + endpoint# + query
+
+response = requests.get(url)
+print(response.json())
