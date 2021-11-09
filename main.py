97dd9d71b1b035c3e9155c5d79d8e9392aea59eb
LukeWarddrip <>
2021-11-09 22:34:02 +0000

    bullgod subbreeds

--- main.py
+++ main.py
@@ -7,4 +7,20 @@ query = ""
 url = url_host + endpoint# + query
 
 response = requests.get(url)
-print(response.json())
+breeds = response.json()["message"]
+
+# get a list of all breeds from the API call
+all_breeds= breeds.keys()
+
+# loop and print out each key
+for breed in all_breeds:
+  print(breed)
+
+# get a list of all subbreeds of a breed with at least 3 sub-breeds but NOT bulldogs
+# Examples include: poodles, retrievers, terriers, mastiffs, spaniels, setters, & more
+
+# Display all bulldog subbreeds
+bulldog_subreeds = breeds["bulldog"]
+input("\nPress enter to get all bulldog subbreeds")
+for sub in bulldog_subreeds:
+  print(sub)
